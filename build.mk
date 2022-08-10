238d5f0b227b93b05dafc0333833ff07a9a3b825
Rogerio Matte Machado <rogermm@gmail.com>
2022-08-10 15:41:18 -0300

    Build Zepplin using Spark versions 3.1.3, 3.2.2

--- build.mk
+++ build.mk
@@ -1,4 +1,7 @@
 Prj([Docker(name="zeppelin",        version="0.10.1-3.1.3", parm={"Z_VERSION": "0.10.1", "SPARK_VERSION": "3.1.3"}),
-     Docker(name="zeppelin-custom", version="0.10.1-3.1.3", parm={"Z_VERSION": "0.10.1", "SPARK_VERSION": "3.1.3"}, depends=["zeppelin:0.10.1"])],
+     Docker(name="zeppelin-custom", version="0.10.1-3.1.3", parm={"Z_VERSION": "0.10.1", "SPARK_VERSION": "3.1.3"}, depends=["zeppelin:0.10.1"]),
+     Docker(name="zeppelin",        version="0.10.1-3.2.2", parm={"Z_VERSION": "0.10.1", "SPARK_VERSION": "3.2.2"}),
+     Docker(name="zeppelin-custom", version="0.10.1-3.2.2", parm={"Z_VERSION": "0.10.1", "SPARK_VERSION": "3.2.2"}, depends=["zeppelin:0.10.1"])
+     ],
      "docker-zeppelin")
 
