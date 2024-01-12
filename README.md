fdaffaa252dd90df4dc429f729fe318919e27522
Rogerio Machado <rogermm@gmail.com>
2024-01-12 16:58:52 -0300

    Add Zeppelin 0.11.0 RC1(not working), change installation method

--- README.md
+++ README.md
@@ -3,8 +3,13 @@
 
 zeppelin.spark.uiWebUrl=https://world-zeppelin.duckdns.org/spark-job/{{PORT}}-{{SERVICE_NAME}}
 
+# Issues
+## Apache Zeppelin 0.10.1 IPython not starting : jupyter-client is not installed
+   *  https://stackoverflow.com/questions/74217783/apache-zeppelin-0-10-1-ipython-not-starting-jupyter-client-is-not-installed
+
 # References
    * https://zeppelin.apache.org/docs/0.10.0/quickstart/kubernetes.html
       * See "Accessing Spark UI (or Service running in interpreter Pod)" 
    * https://github.com/apache/zeppelin/blob/master/docs/quickstart/kubernetes.md
    * https://github.com/apache/zeppelin/commit/2f55fe8ed277b28d71f858633f9c9d76fd18f0c3: [ZEPPELIN-5565] Support Spark 3.2
+   * https://unix.stackexchange.com/questions/699507/how-can-i-install-java-8-on-debian-11-bullseye: How can I install Java 8 on Debian 11 (Bullseye)?
--- build.mk
+++ build.mk
@@ -1,6 +1,15 @@
 Prj([
-     Docker(name="zeppelin",        version="0.10.1-3.2.3", parm={"Z_VERSION": "0.10.1", "SPARK_VERSION": "3.2.3"}),
-     Docker(name="zeppelin-custom", version="0.10.1-3.2.3", parm={"Z_VERSION": "0.10.1", "SPARK_VERSION": "3.2.3"}, depends=["zeppelin:0.10.1"])
+    Docker(name="zeppelin",        version="0.10.1-3.2", parm={"Z_VERSION": "0.10.1", "SPARK_VERSION": "3.2"}),
+    Docker(name="zeppelin-custom", version="0.10.1-3.2", parm={"Z_VERSION": "0.10.1", "SPARK_VERSION": "3.2"})
+
+    #Docker(name="zeppelin",        version="0.11.0-3.2", parm={"Z_VERSION": "0.11.0", "SPARK_VERSION": "3.2"}),
+    #Docker(name="zeppelin-custom", version="0.11.0-3.2", parm={"Z_VERSION": "0.11.0", "SPARK_VERSION": "3.2"}),
+    #Docker(name="zeppelin",        version="0.11.0-3.3", parm={"Z_VERSION": "0.11.0", "SPARK_VERSION": "3.3"}),
+    #Docker(name="zeppelin-custom", version="0.11.0-3.3", parm={"Z_VERSION": "0.11.0", "SPARK_VERSION": "3.3"}),
+    #Docker(name="zeppelin",        version="0.11.0-3.4", parm={"Z_VERSION": "0.11.0", "SPARK_VERSION": "3.4"}),
+    #Docker(name="zeppelin-custom", version="0.11.0-3.4", parm={"Z_VERSION": "0.11.0", "SPARK_VERSION": "3.4"}),
+    #Docker(name="zeppelin",        version="0.11.0-3.5", parm={"Z_VERSION": "0.11.0", "SPARK_VERSION": "3.5"}),
+    #Docker(name="zeppelin-custom", version="0.11.0-3.5", parm={"Z_VERSION": "0.11.0", "SPARK_VERSION": "3.5"})
      ],
      "docker-zeppelin")
 
